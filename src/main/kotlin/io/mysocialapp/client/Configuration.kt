3d6dc92a0b8dc5941c9b294c02ad5a3604a104ad
Romaric Philogene <evoxmusic@gmail.com>
2018-08-30 23:08:20 +0200

    fix: WebSocket configuration overload

--- src/main/kotlin/io/mysocialapp/client/Configuration.kt
+++ src/main/kotlin/io/mysocialapp/client/Configuration.kt
@@ -7,6 +7,6 @@ data class Configuration(private val appId: String,
                          private val apiEndpointURL: String? = null) {
 
     val completeAPIEndpointURL = apiEndpointURL?.let { "$it/api/v1/" } ?: "https://$appId-api.mysocialapp.io/api/v1/"
-    val completeWebSocketEndpointURL = apiEndpointURL?.let { "$it/api/v1/" } ?: "wss://$appId-ws.mysocialapp.io/ws"
+    val completeWebSocketEndpointURL = apiEndpointURL?.let { "$it/ws" } ?: "wss://$appId-ws.mysocialapp.io/ws"
 
 }
